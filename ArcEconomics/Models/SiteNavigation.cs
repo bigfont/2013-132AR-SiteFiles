2e7f8c2cc7cd58836cf11b8e60cb75e60f2994b5
Shaun Luttin <shaunluttin@gmail.com>
2013-11-15 00:56:05 -0800

    Add detection of the breadcrumb parent.

--- ArcEconomics/Models/SiteNavigation.cs
+++ ArcEconomics/Models/SiteNavigation.cs
@@ -5,19 +5,37 @@ using System.Web;
 
 namespace ArcEconomics.Models
 {
-    public class PageLink
+    public class PageLink : IEquatable<PageLink>
     {
+        public string ActionName;
+        public string ControllerName;
+        public string LinkText;
+        public string Path;
         public PageLink(string linkText, string actionName, string controllerName, string path = "")
         {
             this.LinkText = linkText;
             this.ActionName = actionName;
             this.ControllerName = controllerName;
             this.Path = path;
+        }       
+        public override int GetHashCode()
+        {
+            return ActionName.GetHashCode() ^ ControllerName.GetHashCode();
         }
-        public string ActionName;
-        public string ControllerName;
-        public string LinkText;
-        public string Path;
+
+        public override bool Equals(object other)
+        {
+            return this.Equals(other as PageLink);
+        }
+
+        public bool Equals(PageLink other)
+        {
+            return (other != null &&
+                    other.ActionName == this.ActionName &&
+                    other.ControllerName == this.ControllerName &&
+                    other.LinkText == this.LinkText && 
+                    other.Path == this.Path);
+        }
     }
     public class SiteNavigation
     {
